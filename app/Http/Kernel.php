a78c0763a68f58533fe014e93282ba22450dadf3
unknown <mizanursys94@gmail.com>
2020-12-06 02:46:01 +0600

    add to cart fixed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,10 +38,15 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
+        //
+        // 'api' => [
+        //     'throttle:60,1',
+        //     \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        // ],
         'api' => [
+            'bindings',
             'throttle:60,1',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Session\Middleware\StartSession::class,
         ],
     ];
 
